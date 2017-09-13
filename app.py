391b3362421f9cc4be8bcdba99c1f0485481385c
Derek Weitzel <djw8605@gmail.com>
2017-09-13 16:57:23 -0500

    Removing weirdness

--- app.py
+++ app.py
@@ -4,14 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    <h1>Hello from SciTokens!</h1>
+    """.format()
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
