530b794cc3fa1db8fa9115b67a907cc8b7136eff
Derek Weitzel <djw8605@gmail.com>
2017-09-14 12:57:59 -0500

    Fleshing out a bit more

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 from datetime import datetime
 app = Flask(__name__)
 
@@ -9,5 +10,24 @@ def homepage():
     <h1>Hello from SciTokens!</h1>
     """.format()
 
+# Oauth well known    
+@app.route('/.well-known/openid-configuration')
+def OpenIDConfiguration():
+    configuration = {
+        "issuer": "https://demo.scitokens.org",
+        "jwks_uri": "https://demo.scitokens.org/oauth2/certs"
+    }
+    return jsonify(configuration)
+    
+
+# jwks_uri 
+@app.route('/oauth2/certs')
+def Certs():
+    pass
+
+
+
 if __name__ == '__main__':
+    # Given the private key in the ENV PRIVATE_KEY, calculate the public key
+    
     app.run(debug=True, use_reloader=True)
